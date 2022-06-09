f5a2d8168fab305fbd79738a1030d297e5d479f8
Odis Clemons <odis.clemons@gmail.com>
2022-06-09 19:43:57 -0400

    updated apollo server

--- server/server.js
+++ server/server.js
@@ -21,11 +21,11 @@ app.use(routes);
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 };
 
 db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
