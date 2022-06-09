6c473da327717a53422a9110a08477a81034a98b
Odis Clemons <odis.clemons@gmail.com>
2022-06-09 19:42:18 -0400

    added apollo server

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const db = require("./config/connection");
 const routes = require("./routes");
+const { ApolloServer } = require("apollo-server-express");
 
 const app = express();
 const PORT = process.env.PORT || 3002;
@@ -17,6 +18,14 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(routes);
 
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+};
+
 db.once("open", () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
